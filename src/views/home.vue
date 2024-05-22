e4e10a77a916a8f04dec0a54a160103e8ca588f9
Souza <bossviti064@gmail.com>
2024-05-22 11:10:23 -0300

    :sparkles: feat(router): add router and views

---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="flex justify-center mt-40">
+      <img src="../assets/img/fto_seuvoto.svg" alt="logo-foto" />
+    </div>
+    <div>
+      <div class="flex justify-center flex-col mx-auto pt-28">
+        <div class="flex justify-center">
+          <input type="radio" id="termos" />
+          <label for="termos"
+            >Li e estou de acordo com o Termo de Uso e Politica de
+            Privacidade.</label
+          >
+        </div>
+
+        <router-link
+          class="w-36 mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+          to="/state"
+          >ENTRAR</router-link
+        >
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/state.vue
@@ -0,0 +1,61 @@
+<template>
+  <form class="max-w-sm mx-auto">
+    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900"
+      >Selecione o seu Estado</label
+    >
+    <select
+      size="5"
+      name="estado"
+      id="state"
+      class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+    >
+      <option selected>Escolha o seu estado</option>
+      <option v-for="state in states" :value="state.acrState">
+        {{ state.title }}
+      </option>
+    </select>
+    <input
+      class="w-36 mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+      type="submit"
+      value="estado"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      states: [
+        { id: 1, acrState: "AC", title: "Acre" },
+        { id: 2, acrState: "AL", title: "Alagoas" },
+        { id: 3, acrState: "AM", title: "Amazonas" },
+        { id: 4, acrState: "AP", title: "Amapá" },
+        { id: 5, acrState: "BA", title: "Bahia" },
+        { id: 6, acrState: "CE", title: "Ceará" },
+        { id: 7, acrState: "DF", title: "Distrito Federal" },
+        { id: 8, acrState: "ES", title: "Espírito Santo" },
+        { id: 9, acrState: "GO", title: "Goiás" },
+        { id: 10, acrState: "MA", title: "Maranhão" },
+        { id: 11, acrState: "MG", title: "Minas Gerais" },
+        { id: 12, acrState: "MS", title: "Mato Grosso do Sul" },
+        { id: 13, acrState: "MT", title: "Mato Grosso" },
+        { id: 14, acrState: "PA", title: "Pará" },
+        { id: 15, acrState: "PB", title: "Paraíba" },
+        { id: 16, acrState: "PE", title: "Pernambuco" },
+        { id: 17, acrState: "PI", title: "Piauí" },
+        { id: 18, acrState: "PR", title: "Paraná" },
+        { id: 19, acrState: "RJ", title: "Rio de Janeiro" },
+        { id: 20, acrState: "RN", title: "Rio Grande do Norte" },
+        { id: 21, acrState: "RO", title: "Rondônia" },
+        { id: 22, acrState: "RR", title: "Roraima" },
+        { id: 23, acrState: "RS", title: "Rio Grande do Sul" },
+        { id: 24, acrState: "SC", title: "Santa Catarina" },
+        { id: 25, acrState: "SE", title: "Sergipe" },
+        { id: 26, acrState: "SP", title: "São Paulo" },
+        { id: 27, acrState: "TO", title: "Tocantins" },
+      ],
+    };
+  },
+};
+</script>
